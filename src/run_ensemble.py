b6c53f8134faabe1dc012e48413e6bfe483a4a7e
ZoneTsuyoshi <5413.6423.87896744.47449663.ti@gmail.com>
2022-09-23 10:36:18 +0900

    review my effort

--- src/run_ensemble.py
+++ src/run_ensemble.py
@@ -14,6 +14,7 @@ def get_loss_fn(probs, valid_labels, loss_name="f1", th_searh=False):
             return metrics.log_loss(valid_labels, final_probs)
     elif loss_name in ["F1", "f1"]:
         def loss_fn(weight):
+            # weight = weight / weight.sum()
             final_probs = (probs * weight[:,None]).sum(0)
             if th_searh:
                 return -threshold_search(valid_labels, final_probs)["f1"]
@@ -33,15 +34,24 @@ def main(dirpath, ensemble_names, method="Nelder-Mead", loss_name="f1", th_searh
     test_probs = np.array(test_probs_list)
     loss_fn = get_loss_fn(valid_probs, valid_labels, loss_name, th_searh)
     
-    initial_w = np.ones(len(ensemble_names), dtype=float) / len(ensemble_names)
-    cons = ({'type':'eq','fun':lambda w: 1-w.sum()})
-    bounds = [(0,1)]*len(ensemble_names)
-    results = minimize(loss_fn, initial_w, method=method, bounds=bounds, constraints=cons)
-    
-    weight = results["x"]
+    if method=="random":
+        np.random.seed(123)
+        n_cands = 1000
+        cand_w = np.random.rand(n_cands, len(ensemble_names))
+        cand_w = cand_w / cand_w.sum(1)[:,None]
+        scores = np.zeros(n_cands)
+        for i,w in enumerate(cand_w):
+            scores[i] = loss_fn(w)
+        weight = cand_w[np.argmin(scores)]
+    else:
+        initial_w = np.ones(len(ensemble_names), dtype=float) / len(ensemble_names)
+        cons = ({'type':'eq','fun':lambda w: 1-w.sum()})
+        bounds = [(0,1)]*len(ensemble_names)
+        results = minimize(loss_fn, initial_w, method=method, bounds=bounds, constraints=cons)
+        weight = results["x"]
     search_results = threshold_search(valid_labels, (valid_probs * weight[:,None]).sum(0))
     print("best f1: {:.3f}".format(search_results["f1"]))
-    result_dict = {"models":ensemble_names, "target":loss_name, "method":method, "weight":results["x"].tolist(), loss_name:results["fun"], "f1":search_results["f1"], "threshold":search_results["threshold"]}
+    result_dict = {"models":ensemble_names, "target":loss_name, "method":method, "weight":weight.tolist(), "f1":search_results["f1"], "threshold":search_results["threshold"]}
     with open(os.path.join(dirpath, "results.json"), "w") as f:
         json.dump(result_dict, f, indent=4, ensure_ascii=False)
     
